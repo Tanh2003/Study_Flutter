e5804566eab915b9f3464719e198da2d5d670b3f
Tanh2003 <nhoxtuananh092@gmail.com>
2024-05-09 23:40:41 +0700

    video 32

--- study_flutter/lib/main.dart
+++ study_flutter/lib/main.dart
@@ -1,29 +1,38 @@
-
 // Bước 1 : thêm import thư viện
 import 'package:flutter/material.dart';
 
-
- // bước 2 hàm main là hàm chính để chạy app
+// bước 2 hàm main là hàm chính để chạy app
 void main() {
-
   // bước 3 : khởi động app
-  runApp(  MaterialApp(
+  runApp(MaterialApp(
     // Bước :4  sử dụng thành phần để xây dựng giao diện người dùng
     home: SafeArea(
+      child: Scaffold(
+        // hiệu ứng loadding true thì nó xoay xoay false thì trả về State
+        body:Center(child:  MyWidget(true),) 
 
-      child:Scaffold(
-        appBar: AppBar(backgroundColor: Colors.red,
-        title: const Text("Nguyen Tuan Anh"),),
+          // appBar: AppBar(
+          //   backgroundColor: Colors.red,
+          //   title: const Text("Nguyen Tuan Anh"),
+          // ),
+          // body: const Center(
+          //   child: Text("hello word"),
+          // )
 
-    body:const Center(
-
-      child:Text("hello word"),
-
-    ) 
-    ),  
-    ) ,
+          ),
+    ),
     //xóa nhãn debug
     debugShowCheckedModeBanner: false,
- ));
+  ));
 }
 
+class MyWidget extends StatelessWidget {
+  final bool loading;
+
+  MyWidget(this.loading);
+
+  @override
+  Widget build(BuildContext context) {
+    return loading ? const CircularProgressIndicator() : const Text("State");
+  }
+}
