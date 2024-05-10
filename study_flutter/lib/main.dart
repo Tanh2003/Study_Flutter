5cbc3c0563bbe4e648147ec6af0912ba36f6e150
Tanh2003 <nhoxtuananh092@gmail.com>
2024-05-10 09:20:32 +0700

    video 33 - học cách thay đổi trạng thái state  thoong qua setstate

--- study_flutter/lib/main.dart
+++ study_flutter/lib/main.dart
@@ -8,8 +8,10 @@ void main() {
     // Bước :4  sử dụng thành phần để xây dựng giao diện người dùng
     home: SafeArea(
       child: Scaffold(
-        // hiệu ứng loadding true thì nó xoay xoay false thì trả về State
-        body:Center(child:  MyWidget(true),) 
+          // hiệu ứng loadding true thì nó xoay xoay false thì trả về State
+          body: Center(
+        child: MyWidget2(false),
+      )
 
           // appBar: AppBar(
           //   backgroundColor: Colors.red,
@@ -34,5 +36,36 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return loading ? const CircularProgressIndicator() : const Text("State");
+  }
+}
+
+class MyWidget2 extends StatefulWidget {
+  final bool loading;
+  MyWidget2(this.loading);
+  @override
+  State<StatefulWidget> createState() {
+    return MyWidget2State();
+  }
+}
+
+class MyWidget2State extends State<MyWidget2> {
+  late bool _localLoading;
+
+  @override
+  void initState() {
+    _localLoading = widget.loading;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _localLoading
+        ? const CircularProgressIndicator()
+        : FloatingActionButton(onPressed: OnclickButton);
+  }
+
+  void OnclickButton() {
+    setState(() {
+      _localLoading = true;
+    });
   }
 }
